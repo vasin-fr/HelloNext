bb3b94301ffb9c23e57096c1c5a98a4ddf77354a
Oleksii <koliada.oleksii@gmail>
2023-05-01 16:02:14 +0200

    +classnames | +button

--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,2 @@
 export * from './Htag/Htah';
+export * from './Button/Button'
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import { Htag } from '../components';
+import { Button, Htag } from '../components';
 
 export default function Home(): JSX.Element {
 	return (
 		<div>
 			<Htag tag='h1'>Hello</Htag>
+			<Button appearance='primary'>Hello</Button>
+			<Button appearance='ghost'>Ghost</Button>
 		</div>
 	);
 }
